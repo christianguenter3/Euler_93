11bc8c9dbdff3948278788073adda038c76d0a86
christian.guenter <chguenter@web.de>
2013-01-04 15:13:52 +0100

    Solution

--- Euler93.rb
+++ Euler93.rb
@@ -1,29 +1,51 @@
+require 'Set'
 class Euler93
-  def arith_exp
-    digits, operators, brackets, res  = _get_digits("1234"), _get_operators, _get_brackets, []
+  def get_max
+    max_val, max_s = 0, ""
+    (0..9).to_a.combination(4).each do |x|
+      val = arith_exp(x.join.to_s)
+      max_val, max_s = val, x.join.to_s if val > max_val
+    end
+    return max_val, max_s
+  end
+  
+  def arith_exp(input)
+    digits, operators, brackets, res  = _get_digits(input), _get_operators, _get_brackets, []
     digits.each do |n|
       operators.each do |op|
-        #expr = _merge_num_and_op(n, op)
-        #val = eval(expr)
-        #puts "Expr: #{expr} => #{val}" if val % 1 == 0
-        #res << val if val % 1 == 0 && val > 0
+        brackets.each do |br|
+          expr = _merge_num_and_op(n, op, br)
+          val = eval(expr)
+          #puts "Expr: #{expr} => #{val}" if val % 1 == 0
+          res << val if val % 1 == 0 && val > 0
+        end
       end
     end
-    return res.sort.uniq
+    return get_cons_length(res.sort.uniq)
+  end
+
+  def get_cons_length(input)
+    set_i = input.to_set
+    
+    set_i.each do |x| 
+      if set_i.include?(x.to_i.next.to_f)
+      else
+        return x
+      end
+    end
   end
 
   def _merge_num_and_op(n,op,br)
-    nums, operators, brackets, res = n.split(//), op.split(//),brackets.split(//), []
+    nums, operators, brackets, res = n.split(//).reverse, op.split(//).reverse,br.split(//), []
     num_count, x_count = 0,0
     brackets.each_with_index do |x,index|
       if x == "(" || x == ")"
         res << x
       else
         if x_count % 2 == 0
-          res << nums[num_count]
-          num_count += 1
+          res << nums.pop.to_f
         else
-          res << operators[num_count]
+          res << operators.pop
         end
         x_count += 1
       end
@@ -40,17 +62,17 @@ class Euler93
   def _get_operators
     ("-+*/"*3).split(//).combination(3).to_a.uniq.map{|x| x.join.to_s}
   end
-  
+
   def _get_brackets
     res = []
     res << "XXXXXXX"
     res << "(XXX)XXXX"
-    res << "((XXX)XX)XX" 
-    res << "(XXXXX)XX"   
-    res << "XX(XXX)XX"   
-    res << "(XX(XXX))XX" 
+    res << "((XXX)XX)XX"
+    res << "(XXXXX)XX"
+    res << "XX(XXX)XX"
+    res << "(XX(XXX))XX"
     res << "XX((XXX)XX)"
-    res << "XXXXX(XX)"
+    res << "XXXX(XXX)"
     res << "XX(XX(XXX))"
     res << "XX(XXXXX)"
     return res
--- TestEuler93.rb
+++ TestEuler93.rb
@@ -3,7 +3,7 @@ require 'test/unit'
 
 class TestEuler93 < Test::Unit::TestCase
   def test_arith_exp
-    assert_equal(0,@euler93.arith_exp)
+    assert_equal(28,@euler93.arith_exp("1234"))
   end
   
   TEST_MERGE = { ["1234","+++","XXXXXXX"] => "1.0+2.0+3.0+4.0",
@@ -11,7 +11,11 @@ class TestEuler93 < Test::Unit::TestCase
                  ["9328","/*-","XX(XX(XXX))"] => "9.0/(3.0*(2.0-8.0))"}
   
   def test__merge_num_and_op
-    TEST_MERGE.each{|input,output| assert_equal(output,@euler93._merge_num_and_op(input[0],input[1]))}
+    TEST_MERGE.each{|input,output| assert_equal(output,@euler93._merge_num_and_op(input[0],input[1],input[2]))}
+  end
+  
+  def test_get_max
+    assert_equal(51.0,"1258",@euler93.get_max)
   end
   
   def setup 
